e417e1de0d992bb0c071485f8999852be9ff3517
Yang Wang <yang.wang.dec@gmail.com>
2025-03-15 18:33:11 -0400

    printer tets code

--- test/printerTest.js
+++ test/printerTest.js
@@ -1,18 +1,13 @@
 //@ts-check
 
-
-
+import { fileURLToPath } from 'url';
+import { join } from 'path';
+import * as fs from 'node:fs';
 import * as printControl from '../server/controllers/printerController.js'
 
+const dirname = fileURLToPath(new URL('.', import.meta.url));
+const filePath = join(dirname, 'testingText.txt');
 
+const testText = fs.readFileSync(filePath, 'utf-8');
 
-
-// const dirname = fileURLToPath(new URL('.', import.meta.url));
-// const filePath = join(dirname, 'testingText.txt');
-
-
-
-// const testText = fs.readFileSync(filePath, 'utf-8');
-
-
-printControl.printText("hello world!");
+printControl.printText(testText);
